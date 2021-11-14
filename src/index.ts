238dec41e5e4771e5f8861b51913b25cba077d6b
Alexander van Trijffel <alexander@structura.ws>
2021-11-14 18:29:22 +0100

    feat: cli with devices and datapoint commands

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
 import './unhandled-error-handling'
+import { Command } from 'commander'
 import log from './log'
 import envVar from './env'
-import { SetDatapoint } from './localapiclient'
+import { ListDevices, SetDatapoint } from './localapiclient'
 
-log.info('App started')
-;(async () => {
-  const sysap = '00000000-0000-0000-0000-000000000000'
-  const deviceid = 'ABB700D5D898'
-  const channel = 'ch0006'
-  const datapoint = 'idp0000'
-  await SetDatapoint(
-    {
-      baseUrl: String(envVar('LOCALAPI_BASEURL')),
-      user: String(envVar('LOCALAPI_USERNAME')),
-      password: String(envVar('LOCALAPI_PASSWORD')),
-    },
-    { sysapUUID: sysap, deviceId: deviceid, channel, datapoint },
-    '0',
-  )
-})()
+const program = new Command()
+
+const sysap = '00000000-0000-0000-0000-000000000000'
+const conn = {
+  baseUrl: String(envVar('LOCALAPI_BASEURL')),
+  user: String(envVar('LOCALAPI_USERNAME')),
+  password: String(envVar('LOCALAPI_PASSWORD')),
+}
+program
+  .command('devices')
+  .description('list Busch Jaeger devices')
+  .option('-s, --search <search>', 'search filter')
+  .action(async (options) => {
+    const devices = await ListDevices(conn, options.search)
+    if (!devices?.length) {
+      log.info('No devices found')
+      return
+    }
+    log.debug('Devices', { devices })
+  })
+
+const datapoint = program.command('datapoint')
+
+datapoint
+  .command('set')
+  .description('Set datapoint value')
+  .action(async () => {
+    const deviceid = 'ABB700D5D898'
+    const channel = 'ch0006'
+    const datapoint = 'idp0000'
+    await SetDatapoint(conn, { sysapUUID: sysap, deviceId: deviceid, channel, datapoint }, '0')
+  })
+program.parse()
--- src/localapiclient.ts
+++ src/localapiclient.ts
@@ -17,12 +17,79 @@ export interface DatapointRequest {
   datapoint: string
 }
 
+export interface Input {
+  inputId: string
+  value: string
+}
+
+export interface Channel {
+  channelId: string
+  displayName: string
+  inputs: Input[]
+}
+
+export interface Device {
+  deviceId: string
+  name: string
+  channels: Channel[]
+}
+
+const restApiRootUrl = 'fhapi/v1/api/rest'
+
+export const ListDevices = async (conn: SysApConnection, search?: string): Promise<Device[]> => {
+  const requestUrl = `${conn.baseUrl}/${restApiRootUrl}/configuration`
+
+  const response = await axios
+    .get(requestUrl, {
+      headers: {
+        Authorization: authHeader(conn),
+      },
+    })
+    .then((res: AxiosResponse) => res.data)
+    .catch((error) => {
+      log.error(`Request failed`, { error })
+    })
+
+  const searchRegexp = search && new RegExp(search.trim(), 'i')
+
+  const sysApId = Object.keys(response)[0]
+  const { devices } = response[sysApId]
+
+  const mapped = Object.keys(devices)
+    .map((deviceId) => {
+      const device = devices[deviceId]
+      return {
+        deviceId,
+        name: device.displayName,
+        channels: Object.keys(device.channels).map((channelId) => {
+          const channel = device.channels[channelId]
+          return {
+            channelId,
+            displayName: channel.displayName,
+            inputs: Object.keys(channel.inputs).map((inputId) => {
+              const input = channel.inputs[inputId]
+              return { inputId, value: String(input.value) }
+            }),
+          }
+        }),
+      }
+    })
+    .filter(
+      (d) =>
+        !searchRegexp ||
+        d.deviceId.match(searchRegexp) ||
+        d.name.match(searchRegexp) ||
+        d.channels.some((ch) => ch.displayName.match(searchRegexp)),
+    )
+  return mapped
+}
+
 export const SetDatapoint = async (
   conn: SysApConnection,
   request: DatapointRequest,
   value: string | Buffer | ArrayBuffer | Readable,
 ) => {
-  const requestUrl = `${conn.baseUrl}/fhapi/v1/api/rest/datapoint/${request.sysapUUID}/${request.deviceId}.${request.channel}.${request.datapoint}`
+  const requestUrl = `${conn.baseUrl}/${restApiRootUrl}/datapoint/${request.sysapUUID}/${request.deviceId}.${request.channel}.${request.datapoint}`
 
   log.debug(`Setting datapoint`, {
     device: request.deviceId,
